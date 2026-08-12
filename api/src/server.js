7268b44807e5941756cef7796287b401055aeaf6
Richard <r-duerden@live.co.uk>
2026-08-12 13:05:22 +0100

    created the server.js code. Added the cors and error handling plugins w/ comments but need to further review these topics

--- api/src/server.js
+++ api/src/server.js
@@ -10,4 +10,19 @@ const init = async () => {
         port: 4000,
         host: "localhost",
     });
+
+    // Adds the CORS plugin to the server (NTS - Review this topic more in depth)
+    await server.register(corsPlugin);
+
+    // Adds the error handler plugins to the server
+    await server.register(errorHandlerPlugin);
+
+    // Load the routes into the server
+    server.route([...employeeRoutes, ...contractRoutes]);
+
+    // Start hte server
+    await server.start();
+    console.log(`Server running on ${server.info.uri}`);
 };
+
+init();
